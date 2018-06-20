97d906bd844f408797e1b58b6b6baf71721e7241
cchantep <chantepie@altern.org>
2018-06-20 22:28:06 +0200

    Phase #4 - Move txtRecords to reactivemongo.util

--- src/test/scala/UtilSpec.scala
+++ src/test/scala/UtilSpec.scala
@@ -28,69 +28,6 @@ class UtilSpec extends org.specs2.mutable.Specification {
   section("unit")
 
   "DNS resolver" should {
-    import scala.concurrent.duration._
-    import org.xbill.DNS.{ Lookup, Name, Resolver, Type }
-
-    val timeout = 5.seconds
-
-    def defaultResolver: Resolver = {
-      val r = Lookup.getDefaultResolver
-      r.setTimeout(timeout.toSeconds.toInt)
-      r
-    }
-
-    def srvRecords(
-      name: String,
-      srvPrefix: String, // e.g. "_mongodb._tcp"
-      resolver: Resolver = defaultResolver): List[String] = {
-      val service = Name.fromConstantString(name + '.')
-      // assert service.label >= 3
-
-      val baseName = Name.fromString(
-        name.dropWhile(_ != '.').drop(1), Name.root)
-
-      val srvName = Name.concatenate(
-        Name.fromConstantString(srvPrefix), service)
-
-      val lookup = new Lookup(srvName, Type.SRV)
-
-      lookup.setResolver(resolver)
-
-      lookup.run().map { rec =>
-        val nme = rec.getAdditionalName
-
-        // if nme.isAbsolute then assert nme.subdomain(baseName)
-
-        if (nme.isAbsolute) {
-          nme.toString(true)
-        } else {
-          Name.concatenate(nme, baseName).toString(true)
-        }
-      }.toList
-    }
-
-    def txtRecords(
-      name: String,
-      resolver: Resolver = defaultResolver): List[String] = {
-
-      val lookup = new Lookup(name, Type.TXT)
-
-      lookup.setResolver(resolver)
-
-      lookup.run().map { rec =>
-        val data = rec.rdataToString
-        val stripped = data.stripPrefix("\"")
-
-        if (stripped == data) {
-          data
-        } else {
-          stripped.stripSuffix("\"")
-        }
-      }.toList
-    }
-
-    // ---
-
     "resolve SRV record for _imaps._tcp at gmail.com" in {
       reactivemongo.util.srvRecords(
         name = "gmail.com",
@@ -99,8 +36,8 @@ class UtilSpec extends org.specs2.mutable.Specification {
     } tag "srvRecords"
 
     "resolve TXT record for gmail.com" in {
-      todo /* test that calling txtRecords for gmail.com returns the following:
-            List("v=spf1 redirect=_spf.google.com") */
+      reactivemongo.util.txtRecords(
+        "gmail.com") must_=== List("v=spf1 redirect=_spf.google.com")
     } tag "txtRecords"
   }
 }
